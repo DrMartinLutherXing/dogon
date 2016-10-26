b5bc9fd2ed29744b54b7bd97cb4e9e836dd0900c
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-10-26 16:43:15 -0700

    ctinit;

---
+++ js/core/config.js
@@ -0,0 +1,11 @@
+core.config = {
+    "header": {
+        "logo": "Your Logo", 
+        "right": []
+    }, 
+    "log": {
+        "exclude": [], 
+        "include": []
+    }, 
+    "css": []
+};
